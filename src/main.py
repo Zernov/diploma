fa016e85a584eeeb218dbf85ba1aac26ab138bfd
alvzernov <alvzernov@gmail.com>
2017-04-16 21:42:37 +0300

    normalization params

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 import parser
 import pretty
 import sys
+import morph
+import pretty
+
 
 from parser import MFD
 from parser import readNews
 from pretty import groupArgs
+from pretty import writeData
 from sys import argv
+from morph import normalizeString
+from pretty import printProgress
 
 args = groupArgs(sys.argv)
 
@@ -15,8 +21,9 @@ def parse():
         source = MFD()
 
     if source != None:
-        news = source.getNews(args['-c'], args['-o'])
+        news = source.getNews(args['-c'])
 
+    writeData(news, args['-o'])
     return news
 
 def read():
@@ -24,7 +31,23 @@ def read():
     news = readNews(args['-i'])
     return news
 
+def norm(data):
+    print('Normalizing news from {}.\nOutput file: {}'.format(args['-i'] if args.get('-i') != None else args['-s'], args['-n']))
+    news = {}
+    i = 0
+    for item in data:
+        news.update({ item : normalizeString(data[item]) })
+        printProgress(i, len(data))
+        i += 1
+
+    printProgress(i, len(data), True)
+    writeData(news, args['-n'])
+    return news
+
 if args['-'] == 'p':
     news = parse()
 elif args['-'] == 'r':
     news = read()
+
+if args.get('-n') != None:
+    norm(news)
--- src/parser.py
+++ src/parser.py
@@ -4,17 +4,18 @@ import re
 import time
 import functools
 import html2text
+import pretty
 
+from pretty import printProgress
+from pretty import writeData
 from bs4 import BeautifulSoup
 
-def printProgress(current, total, new_line = False):
-    print('[{}%] {}/{}'.format(str(int(100 * current / total)), str(current), str(total)), end = '\n' if new_line else '\r')
 
 class MFD:
     domain = 'http://mfd.ru'
     prefix = 'http://mfd.ru/news/company/view/?id='
 
-    def getNews(self, company, output):
+    def getNews(self, company):
         url = self.prefix + company
         page = urllib.request.urlopen(url)
         data = BeautifulSoup(page, 'lxml').find('table', { 'id' : 'issuerNewsList' }).findAll('a')
@@ -31,7 +32,7 @@ class MFD:
             i = 0
 
             while i < (len(item_data) - 2):
-                item_string += item_data[i].getText() + '\n'
+                item_string += item_data[i].getText() + ' '
                 i += 1
 
             news.update({ item_url : item_string })
@@ -39,20 +40,6 @@ class MFD:
             time.sleep(0.1)
 
         printProgress(current, total, True)
-        print('Complete!')
-
-        output_file = open(output, 'w+')
-
-        i = 0
-        for item in news:
-            if i != (len(news) - 1):
-                output_file.write('{}\n\n{}\n\n\n'.format(str(item), str(news[item])))
-            else:
-                output_file.write('{}\n\n{}'.format(str(item), str(news[item])))
-            i += 1
-
-        output_file.close()
-
         return news
 
 def readNews(path):
