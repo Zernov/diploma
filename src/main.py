5e8db210961817a6848fc603a1b5d10e6c2724ef
alvzernov <alvzernov@gmail.com>
2017-05-08 22:48:40 +0300

    almooost

--- src/main.py
+++ src/main.py
@@ -52,98 +52,94 @@ datet = '08/05/2017'
 
 #region Learning
 num_words = 3000
-dropout_rate = 0.8
+dropout_rate = 0.5
 dimension = 64
 l1_rate = 0.1
 l2_rate = 0.1
 l_rate = 0.01
 batch_size = 1
-epochs = 128
+epochs = 2
 validation_split = 0.1
 #endregion
 
 #region News Getter
-news_dates, news, news_count = downloadNews(company, amount)
-writeNews(news_dates, news, news_count, path + 'news/{}.csv'.format(company))
+#news_dates, news, news_count = downloadNews(company, amount)
+#writeNews(news_dates, news, news_count, path + 'news/{}.csv'.format(company))
 #news_dates, news, news_count = readNews(path + 'news/{}.csv'.format(company))
 #endregion
 
 #region Stock Getter
-stocks_dates, stocks, stocks_count = downloadStock(company, datef, datet)
-writeStock(stocks_dates, stocks, stocks_count, path + 'stocks/{}.csv'.format(company))
+#stocks_dates, stocks, stocks_count = downloadStock(company, datef, datet)
+#writeStock(stocks_dates, stocks, stocks_count, path + 'stocks/{}.csv'.format(company))
 #stocks_dates, stocks, stocks_count = readStock(path + 'stocks/{}.csv'.format(company))
 #endregion
 
 #region Stemmer
-stems_dates, stems, stems_count = stem(news_dates, news, news_count)
-writeNews(stems_dates, stems, stems_count, path + 'stems/{}.csv'.format(company))
+#stems_dates, stems, stems_count = stem(news_dates, news, news_count)
+#writeNews(stems_dates, stems, stems_count, path + 'stems/{}.csv'.format(company))
 #stems_dates, stems, stems_count = readNews(path + 'stems/{}.csv'.format(company))
 #endregion
 
 #region Connector
-connections_dates, connections_news, connections_stocks, connections_count = connect(stems_dates, stems, stems_count, stocks_dates, stocks, stocks_count)
-writeConnections(connections_dates, connections_news, connections_stocks, connections_count, path + 'connections/{}.csv'.format(company))
-#connections_dates, connections_news, connections_stocks, connections_count = readConnections(path + 'connections/{}.csv'.format(company))
+#connections_dates, connections_news, connections_stocks, connections_count = connect(stems_dates, stems, stems_count, stocks_dates, stocks, stocks_count)
+#writeConnections(connections_dates, connections_news, connections_stocks, connections_count, path + 'connections/{}.csv'.format(company))
+connections_dates, connections_news, connections_stocks, connections_count = readConnections(path + 'connections/{}.csv'.format(company))
 #endregion
 
-#region Work
-tokenizer = Tokenizer(num_words=num_words)
-tokenizer.fit_on_texts(texts=connections_news)
+def fit(dates, news, stocks, count, name):
 
-min_stock = minArray(connections_stocks)
-max_stock = maxArray(connections_stocks)
+    tokenizer = Tokenizer(num_words=num_words)
+    tokenizer.fit_on_texts(texts=connections_news)
 
-total_dates = connections_dates
-total_news = tokenizer.texts_to_sequences(connections_news)
-total_stocks = normalizeArray(connections_stocks, min_stock, max_stock)
-total_count = connections_count
+    total_dates = connections_dates
+    total_news = tokenizer.texts_to_sequences(connections_news)
+    total_stocks = connections_stocks
+    total_count = connections_count
 
-total_news_sequence = sequence.pad_sequences(sequences=total_news)
+    total_news_sequence = sequence.pad_sequences(sequences=total_news)
 
-border = int(connections_count * 0.75)
+    border = int(connections_count * 0.75)
 
-training_dates = total_dates[:border]
-training_news = total_news_sequence[:border]
-training_stocks = total_stocks[:border]
-training_count = border
+    training_dates = total_dates[:border]
+    training_news = total_news_sequence[:border]
+    training_stocks = total_stocks[:border]
+    training_count = border
 
-testing_dates = total_dates[border:]
-testing_news = total_news_sequence[border:]
-testing_stocks = total_stocks[border:]
-testing_count = total_count - border
+    testing_dates = total_dates[border:]
+    testing_news = total_news_sequence[border:]
+    testing_stocks = total_stocks[border:]
+    testing_count = total_count - border
 
-training_X = numpy.array(training_news)
-training_y = numpy.array(training_stocks)
+    training_X = numpy.array(training_news)
+    training_y = numpy.array(training_stocks)
 
-testing_X = numpy.array(testing_news)
-testing_y = numpy.array(testing_stocks)
-'''
-print(len(testing_y))
-print(testing_y)
+    testing_X = numpy.array(testing_news)
+    testing_y = numpy.array(testing_stocks)
 
-model = load_model(path + 'models/{}_model-01.h5'.format(company))
+    model = Sequential()
+    model.add(Embedding(input_dim=num_words, output_dim=dimension))
+    model.add(LSTM(units=dimension))
+    model.add(Dropout(rate=dropout_rate))
+    model.add(Dense(units=1, kernel_regularizer=l1_l2(l1=l1_rate, l2=l2_rate)))
+    model.add(Activation(activation='sigmoid'))
+    model.compile(optimizer=Adam(lr=l_rate), loss=binary_crossentropy, metrics=[binary_accuracy])
 
-get_y = model.predict(testing_X)
+    hist = model.fit(training_X, training_y, batch_size=batch_size, epochs=epochs, validation_split=validation_split)
+    model.save(path + 'models/{}_model-{}.h5'.format(company, name))
 
-print(len(get_y))
-print(get_y)
+    with open(path + 'models/{}_history-{}.txt'.format(company, name), 'w+', encoding='utf8') as temp:
+        temp.write(str(hist.history))
 
-model = Sequential()
-model.add(Embedding(input_dim=num_words, output_dim=dimension))
-model.add(LSTM(units=dimension))
-model.add(Dropout(rate=dropout_rate))
-model.add(Dense(units=1, kernel_regularizer=l1_l2(l1=l1_rate, l2=l2_rate)))
-model.add(Activation(activation='sigmoid'))
-model.compile(optimizer=Adam(lr=l_rate), loss=binary_crossentropy, metrics=[binary_accuracy])
+    score = model.evaluate(testing_X, testing_y, batch_size=batch_size)
+    with open(path + 'models/{}_score-{}.txt'.format(company, name), 'w+', encoding='utf8') as temp:
+        temp.write(str(score))
 
-hist = model.fit(training_X, training_y, batch_size=batch_size, epochs=epochs, validation_split=validation_split)
-model.save(path + 'models/{}_model-10.h5'.format(company))
+def predict(X, name):
 
-with open(path + 'models/{}_history-10.txt'.format(company), 'w+', encoding='utf8') as temp:
-    temp.write(str(hist.history))
+    model = load_model(path + 'models/{}_model-{}.h5'.format(company, name))
 
-score = model.evaluate(testing_X, testing_y, batch_size=batch_size)
-with open(path + 'models/{}_score-10.txt'.format(company), 'w+', encoding='utf8') as temp:
-    temp.write(str(score))
-'''
-#endregion
+    result = model.predict(X)
+
+    return result
+
+fit(connections_dates, connections_news, connections_stocks, connections_count, '11')
